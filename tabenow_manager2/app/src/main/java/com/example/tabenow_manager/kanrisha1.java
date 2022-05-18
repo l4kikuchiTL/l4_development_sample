372e885af45a7679d9f33f22953d6849dbb79f74
L4-ono <105625438+L4-ono@users.noreply.github.com>
2022-05-18 11:01:42 +0900

    Add files via upload

---
+++ tabenow_manager2/app/src/main/java/com/example/tabenow_manager/kanrisha1.java
@@ -0,0 +1,14 @@
+package com.example.tabenow_manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kanrisha1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kanrisha1);
+    }
+}
